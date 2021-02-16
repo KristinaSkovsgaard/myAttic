acdf936256601dd13d5ce8cc768efc5547908a90
Kristina Skovsgaard <kristinaskovsgaard@gmail.com>
2021-02-16 12:53:56 +0100

    Add category and list categories

---
+++ myAttic/Controller/CategoryViewController.swift
@@ -0,0 +1,90 @@
+//
+//  CategoryViewController.swift
+//  myAttic
+//
+//  Created by App og Blog on 16/02/2021.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+    
+    var categoriesArray = [Category]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoriesArray.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
+        
+        let category = categoriesArray[indexPath.row]
+        cell.textLabel?.text = category.name
+        cell.accessoryType = .disclosureIndicator
+
+        return cell
+    }
+    
+
+    //MARK: - Add item Button
+    
+    @IBAction func addCategoryPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new Category", message: "", preferredStyle: .alert)
+    
+        let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
+            
+            let newCategory = Category(context:self.context)
+            newCategory.name = textField.text!
+            
+            self.categoriesArray.append(newCategory)
+            
+            self.saveCategories()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new category"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK: - Data Manipulation
+    
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()){
+        do{
+            categoriesArray = try context.fetch(request)
+        }catch{
+            print("Error fetching data from context \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func saveCategories(){
+        do{
+            try context.save()
+        }catch{
+            print("Error saving context \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+
+}
